51fa56af9f11e19a7116aa490f539de81864e359
MurzalinAsan <murzalinasan@gmail.com>
2025-01-24 17:38:19 +0500

    Organized files into lab1 subdirectory

---
+++ lab1/16.py
@@ -0,0 +1,18 @@
+x = int(1)
+y = int(3.1) #y will be 2
+z = int("3") #z will be 3
+
+#Floats
+
+x = float(1) #x is 1.0
+
+y = float(3.8) #y will be 3.8
+
+z = float("4.1") #z will be 4.1
+
+
+#with strings
+
+x = str("k2") # x will be 'k2'
+y = str(4)    # y will be '4'
+z = str(2.0)  # z will be '2.0'
---
+++ lab1/6.py
@@ -0,0 +1,8 @@
+#This is a comment
+print("Hello, KBTU")
+
+"""
+this comment is written
+in multiple lines
+.
+"""
